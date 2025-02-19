585f06ca0635f9069124a592fcfcdf150588e01e
ShephardOS <moleg7969@gmail.com>
2025-02-19 20:17:47 +0500

    Update install-boxwine.sh

--- install-boxwine.sh
+++ install-boxwine.sh
@@ -1,11 +1,11 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-# Установка зависимостей
+# Обновление и установка зависимостей
 pkg update -y
 pkg upgrade -y
 pkg install x11-repo -y
 pkg install tur-repo -y
-pkg install box64 box86 wine xwayland -y
+pkg install box64 box86 wine xwayland curl -y
 
 # Создание рабочего каталога
 mkdir -p $HOME/boxwine
@@ -13,6 +13,8 @@ cd $HOME/boxwine
 
 # Скачивание BoxWine
 curl -L -o boxwine.tar.gz https://github.com/ShephardOS/BoxWine/releases/latest/download/boxwine.tar.gz
+
+# Распаковка и удаление архива
 tar -xvf boxwine.tar.gz
 rm boxwine.tar.gz
 
