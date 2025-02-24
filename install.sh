139b2219d4cc9466a010b2a9a95e66d401bb289f
ShephardOS <moleg7969@gmail.com>
2025-02-24 05:52:06 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,38 +1,82 @@
 
 #!/bin/bash
 
-rm -f ~/x clear echo " by ShephardOS "
+rm ~/x
+echo "                                                                     ";
+echo "                                                                   / ";
+echo "                                                                 /  ";
+echo "     ____  .    ,    ____     ____     ____     ____     _____,/    ";
+echo "   /'    )  \  /   /'    )--/'    )--/'    )--)'    )--/'    /      ";
+echo " /(___,/'    \'   '---,   /'       /'    /' /'       /'    /        ";
+echo "(__________/' \_(___,/   (___,/   (___,/' /'        (___,/(__        ";
+echo "                                                                     ";
+echo " by ShephardOS             ";
+echo "                                                                     ";
 
-echo "Installing termux-am..." pkg install termux-am -y &>/dev/null pkg install wget -y &>/dev/null clear
+echo "Installing termux-am"
+pkg install termux-am -y &>/dev/null
+yes | pkg install wget; clear
+termux-setup-storage & sleep 4 &>/dev/null
 
-termux-setup-storage &>/dev/null sleep 4
+while true; do
+    if [ -d ~/storage/shared ]; then
+        break
+    else
+        echo "Storage permission denied"
+    fi
+    sleep 3
+done
 
-while true; do if [ -d ~/storage/shared ]; then break else echo "Storage permission denied" fi sleep 3 done
+echo "Installing termux packages"
+apt-get clean
+apt-get update >/dev/null 2>&1
+apt-get -y --with-new-pkgs -o Dpkg::Options::="--force-confdef" upgrade >/dev/null 2>&1
+pkg install x11-repo -y &>/dev/null
+pkg install pulseaudio -y &>/dev/null
+pkg install xwayland -y &>/dev/null
+pkg install wget -y &>/dev/null
+pkg install tsu -y &>/dev/null
+pkg install root-repo -y &>/dev/null
+pkg install patchelf -y &>/dev/null
+pkg install p7zip -y &>/dev/null
+pkg install xorg-xrandr -y &>/dev/null
+pkg install ncurses-utils -y &>/dev/null
+pkg install hashdeep -y &>/dev/null
+pkg install termux-x11-nightly -y &>/dev/null
 
-echo "Installing termux packages..." apt-get clean apt-get update >/dev/null 2>&1 apt-get -y --with-new-pkgs -o Dpkg::Options::="--force-confdef" upgrade >/dev/null 2>&1 pkg install x11-repo -y &>/dev/null pkg install pulseaudio -y &>/dev/null pkg install xwayland -y &>/dev/null pkg install tsu -y &>/dev/null pkg install root-repo -y &>/dev/null pkg install patchelf -y &>/dev/null pkg install p7zip -y &>/dev/null pkg install xorg-xrandr -y &>/dev/null pkg install ncurses-utils -y &>/dev/null pkg install hashdeep -y &>/dev/null pkg install termux-x11-nightly -y &>/dev/null
+if [ -e $PREFIX/glibc ]; then
+    echo -n "Removing previous glibc. Continue? (Y/n) "
+    read i
+    if [ "$i" = "Y" ] || [ "$i" = "y" ]; then
+        rm -rf $PREFIX/glibc
+    else
+        return 1
+    fi
+fi
 
-if [ -e "$PREFIX/glibc" ]; then echo -n "Removing previous glibc. Continue? (Y/n) " read -r i if [ "$i" = "Y" ] || [ "$i" = "y" ]; then rm -rf "$PREFIX/glibc" else exit 1 fi fi
+echo "Installing BoxWine"
 
-echo "Installing BoxWine..." mkdir -p "$PREFIX/glibc/opt/package-manager/installed"
+function wget-git-q {
+    wget -q --retry-connrefused --tries=0 --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" "https://gitlab.com/api/v4/projects/$PROJECT_ID/repository/files/$1/raw?ref=main" -O $2
+    return $?
+}
 
-echo "PRIVATE-TOKEN=glpat-kj--11qKof3MZXBayzRV PROJECT_ID=64704412" > "$PREFIX/glibc/opt/package-manager/token"
+echo "Updating package manager"
+mkdir -p $PREFIX/glibc/opt/package-manager/installed
 
-. "$PREFIX/glibc/opt/package-manager/token"
+echo "PRIVATE_TOKEN=glpat-kj--11qKof3MZXBayzRV
+PROJECT_ID=64704412">$PREFIX/glibc/opt/package-manager/token
 
-wget -q --retry-connrefused --tries=0 --header "PRIVATE-TOKEN: $PRIVATE-TOKEN" 
-"https://gitlab.com/api/v4/projects/$PROJECT_ID/repository/files/package-manager/raw?ref=main" 
--O "$PREFIX/glibc/opt/package-manager/package-manager"
-
-if [ $? -ne 0 ]; then echo "Download failed" exit 1 fi
-
-. "$PREFIX/glibc/opt/package-manager/package-manager" sync-all
+. $PREFIX/glibc/opt/package-manager/token
+if ! wget-git-q "package-manager" "$PREFIX/glibc/opt/package-manager/package-manager"; then
+    echo "Download failed"
+    return 1
+fi
+. $PREFIX/glibc/opt/package-manager/package-manager
+sync-all
 
 sync-package wine-9.3-vanilla-wow64
 
-ln -sf "$PREFIX/glibc/opt/scripts/boxwine" "$PREFIX/bin/boxwine" echo "To start - type 'boxwine'"
-
-Curl command to download and execute the script
-
-echo "To install, run:" echo "curl -sSL https://yourserver.com/install_boxwine.sh | bash"
-
+ln -sf $PREFIX/glibc/opt/scripts/BoxWine $PREFIX/bin/BoxWine
+echo "To start - type \"BoxWine\""
 
